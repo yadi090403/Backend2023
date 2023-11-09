fbebd35976c74de1363f01bf2c14816865521ea8
yadi090403 <yadielyadi365@gmail.com>
2023-11-09 20:52:07 +0700

    praktikum_pertemuan-6

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+// tugas pertemuan4
 // method GET
 Route::get('/animals', [AnimalController::class, "index"] );
 
@@ -30,3 +33,19 @@ route::put('/animals/{id}', [AnimalController::class, "update"] );
 
 // method delete
 route::delete('/animals/{id}', [AnimalController::class, "destroy"] );
+
+# Route students
+// tugas praktikum 5-6
+// method get studens
+Route::get('/students', [StudentController::class, 'index']);
+// method post studens
+Route::post('/students', [StudentController::class, 'store']);
+// method update studens
+Route::put('/students/{id}', [StudentController::class, 'update']);
+// method delete studens
+Route::delete('/students/{id}', [StudentController::class, 'destroy']);
+
+
+# Method GET
+Route::get('/students', [StudentController::class, 'index']);
+Route::get('/students/{id}', [StudentController::class, 'show']);

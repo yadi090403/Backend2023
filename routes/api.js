b9c57e3e4f37e667c72a66b4296da780498f96e7
yadi090403 <yadielyadi365@gmail.com>
2024-01-05 08:43:17 +0700

    tugas pertemuan12

--- routes/api.js
+++ routes/api.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 // make home routing
 router.get("/", (req, res) => {
-    res.send("Hello Mahmudi.....?");
+    res.send("Hello Mahmudi Apakabar....");
 });
 
 // Routing for Students
@@ -17,6 +17,7 @@ router.get("/students", StudentController.index);
 router.post("/students", StudentController.store);
 router.put("/students/:id", StudentController.update);
 router.delete("/students/:id", StudentController.destroy);
+router.get("/students/:id", StudentController.show);
 
 // export routing
 module.exports = router;
